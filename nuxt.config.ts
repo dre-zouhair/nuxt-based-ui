5813b7eeee8b835be522ec8d88599bfc2b6639a7
Zouhair DRIOUECHE <dre.zouhair@gmail.com>
2024-06-02 14:40:09 +0100

    add base NuxtConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: {
+        enabled: true,
+        timeline: {
+            enabled: true
+        }
+    },
+    ssr: false,
+    runtimeConfig: {
+        // The private keys which are only available within server-side
+        apiSecret: process.env.API_SECRET,
+        // Keys within public, will be also exposed to the client-side
+        public: {
+            apiBase: process.env.API_BASE
+        }
+    }
 })
