e509cccce7a38d1d47dc18d6a7a053614437be4e
Zouhair DRIOUECHE <dre.zouhair@gmail.com>
2024-06-02 16:29:02 +0200

    Add Component

---
+++ components/OverlayLoader.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="overlay">
+    <div class="overlay__inner">
+      <div class="overlay__content">
+        <!--        Add a Loader here-->
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
