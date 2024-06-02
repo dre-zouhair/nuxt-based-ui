66d9b64b6c09e357fa5e342d237748f69db1f1d9
Zouhair DRIOUECHE <dre.zouhair@gmail.com>
2024-06-02 15:11:08 +0100

    Add Layouts

---
+++ layouts/unauthenticated.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>Unauthenticated Layout</h1>
+  <slot />
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+definePageMeta({
+  layout: 'unauthenticated'
+});
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
